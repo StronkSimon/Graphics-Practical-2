635de0c831bd3636db8811a68145cfa9113812a2
yoerihoeb <yoerihoeb@gmail.com>
2024-06-28 13:18:24 +0200

    comments toegevoegd

--- shaders/fs.glsl
+++ shaders/fs.glsl
@@ -18,20 +18,20 @@ out vec4 outputColor;
 
 void main()
 {
-	vec3 norm = normalize(normalWorld.xyz);
+	vec3 norm = normalize(normalWorld.xyz); //get the normal
 	
-	vec3 camDirection = normalize(cameraPosition - positionWorld.xyz);
+	vec3 camDirection = normalize(cameraPosition - positionWorld.xyz);		//calculate the camera direction
 
-	vec3 lightDirection = normalize(lightPosition - positionWorld.xyz);
+	vec3 lightDirection = normalize(lightPosition - positionWorld.xyz);		//calculate the light direction
 
-	float diff = max(0,dot(norm, lightDirection));
+	float diff = max(0,dot(norm, lightDirection));		//calculate the diffuse part of the formula
 	vec3 diffuse = diff * lightColor;
 
-	vec3 reflectDirection = reflect(-lightDirection, norm);
-	float spec = pow(max(dot(camDirection,reflectDirection),0.0),32);
+	vec3 reflectDirection = reflect(-lightDirection, norm);		//calculate the specular part of the formula
+	float spec = pow(max(dot(camDirection,reflectDirection),0.0),100);
 	vec3 specular = 0.5*spec*lightColor;
 
-	vec3 result = diffuse + specular + ambientLightColor;
+	vec3 result = diffuse + specular + ambientLightColor;		//add everything together with the ambient light
 
-	outputColor = vec4(result * (texture(diffuseTexture,uv).rgb), 1.0);
+	outputColor = vec4(result * (texture(diffuseTexture,uv).rgb), 1.0);		//mix it with the texture
 }
